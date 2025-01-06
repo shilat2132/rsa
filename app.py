8f85646d18c3ecb0c7f2ab546ba5ec504c3f25f2
shilat2132 <shilat2132@gmail.com>
2025-01-06 16:04:35 +0200

    מלמ

--- app.py
+++ app.py
@@ -6,21 +6,21 @@ from tm import Tm
 from basicMachines.remainderMachine import remainderPQ
 from rsa.gcd import Gcd
 
-a = ["1" for i in range(24)]
-b = ["1" for i in range(21)]
+a = ["1" for i in range(26)]
+b = ["1" for i in range(7)]
 
 # remainder
-m = remainderPQ([a, b])
-m.runMachine()
-print("the machine in the end: \n{m}".format(m=m))
+# m = remainderPQ([a, b])
+# m.runMachine()
+# print("the machine in the end: \n{m}".format(m=m))
 
 # gcd
 
-# gc = Gcd([a, b])
-# gc.runGcdAbstract()
-# print("the result of gcd: {d}".format(d= gc.d()))
-# print("the result of t: {t}".format(d= gc.t()))
-# print("the result of s: {t}".format(s= gc.s()))
+gc = Gcd([a, b])
+gc.runGcdAbstract()
+print("the result of gcd: {d}".format(d= gc.d()))
+print("the result of t: {t}".format(t= gc.t()))
+print("the result of s: {s}".format(s= gc.s()))
 
 
 
@@ -33,9 +33,9 @@ print("the machine in the end: \n{m}".format(m=m))
 # phiN(p, q)
 
 # div
-d= divMachine([a, b])
-d.runMachine()
-print("the machine of division: {d}".format(d=d))
+# d= divMachine([a, b])
+# d.runMachine()
+# print("the machine of division: {d}".format(d=d))
 
 # copy
 # Tm.copyTape(a, b)
